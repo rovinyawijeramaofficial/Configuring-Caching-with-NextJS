f3d3d963b67fce3d294d42b7d548741a3537a3bd
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-10-09 22:24:57 +0530

    Updated

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,12 +1,15 @@
 import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+  // const response = await fetch('http://localhost:8080/messages', {
+  //   headers: {
+  //     'X-ID': 'page',
+  //   },
+  // });
+  // const messages = await response.json();
+
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
